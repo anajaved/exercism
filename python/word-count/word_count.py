fceb7b6db5b0fd7c256243a3114dd67dd6619ba9
Ana Javed <ana.javed.3@gmail.com>
2017-05-19 14:48:25 -0400

    Gives counts for words in sentences

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,16 +1,26 @@
+import collections
+
 def word_count(sentence):
+	'''
+	function takes in a sentence, and returns a dictionary
+	of each word's count. Takes in account words with 
+	unnecessary punctuations & removes them. 
+	'''
+
 	sentence_dict={}
 	sentence=sentence.lower()
-	sentence=sentence.replace("_", " ")
+	punctuation = ["_", ".",",","!","&","@","%","^", "$", ":"]
+	for each in punctuation:
+		sentence=sentence.replace(each, " ")
+
 	for word in sentence.split():
 		if word in sentence_dict:
 			sentence_dict[word] +=1 
 		else:
 			sentence_dict[word] =1
-	print sentence_dict
+	return sentence_dict 
 
 
 
-word_count('one of each')
-word_count('one fish two fish red fish blue fish')
-word_count('wait for       it')
+word_count('car : carpet as java : javascript!!&@$%^&')
+word_count('hey,my_spacebar_is_broken.')
