edf58f7c8585cc45d30338aced5e4d00e5a93468
Ana Javed <ana.javed.3@gmail.com>
2017-05-23 17:29:22 -0400

    added docstrings to function

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,49 +1,54 @@
 def hey(statement):
-    punct = [" ", ",", "-", "\'", ":", ")", "("]
-    for each in punct:
-        statement = statement.replace(each, "")
+	'''
+	Input: string
+	Output: returns appropriate response in string format
 
-    if statement.endswith("?"):
-    	statement= statement.strip("?")
-    	if (statement.upper() == statement) & (len(statement)>0):
-    		try:
-    			int(statement) == True
-    			return "Sure."
-    		except:
-    			return "Whoa, chill out!"
-    	else:
-    		return "Sure."
+	Function Bob answers 'Sure.' if you ask him a question.
+	He answers 'Whoa, chill out!' if you yell at him.
+	He says 'Fine. Be that way!' if you address him without actually saying anything.
+	He answers 'Whatever.' to anything else.
+	'''
 
-    elif statement.endswith("!"):
-    	if (statement.upper() == statement):
-    		return "Whoa, chill out!"
-    	else:
-    		return "Whatever."
 
-    elif (len(statement) == 0) | ("\t" in statement):
-    	return "Fine. Be that way!"
+	punct = [" ", ",", "-", "\'", ":", ")", "("]
+	for each in punct:
+		statement = statement.replace(each, "")
 
-    elif statement.upper() == statement:
-        try:
-            int(statement)
-            return "Whatever."
-        except:
-            return "Whoa, chill out!"
+	if statement.endswith("?"):
+		statement= statement.strip("?")
+		if (statement.upper() == statement) & (len(statement)>0):
+			try:
+				int(statement) == True
+				return "Sure."
+			except:
+				return "Whoa, chill out!"
+		else:
+			return "Sure."
 
-    else:
-    	return "Whatever."
+	elif statement.endswith("!"):
+		if (statement.upper() == statement):
+			return "Whoa, chill out!"
+		else:
+			return "Whatever."
+
+	elif (len(statement) == 0) | ("\t" in statement):
+		return "Fine. Be that way!"
+
+	elif statement.upper() == statement:
+		try:
+			int(statement)
+			return "Whatever."
+		except:
+			return "Whoa, chill out!"
+
+	else:
+		return "Whatever."
 
 
 
 hey("Let's go make out behind the gym!") #Whatever.
-
-hey("WATCH OUT!") #chill out
-
-hey("1, 2, 3") #whatever.
-
-hey("4?") #sure`
-
+hey("WATCH OUT!") #Whoa, chill out!
+hey("1, 2, 3") #Whatever.
+hey("4?") #Sure
 hey("") #"Fine. Be that way!"
-
-
 
